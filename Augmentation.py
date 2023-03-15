abcac988f8e7a7981827d31cdda602861891e112
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2023-03-15 17:30:35 +0100

    Updated DataLoader

--- Augmentation.py
+++ Augmentation.py
@@ -6,3 +6,28 @@ def augment_Data_TD():
 
 def augment_Data_FD():
     raise(NotImplementedError)
+
+def augment_Data(self, XData, yData, subset_range = None,
+             jitter = False, jitter_range = None,
+             phase = False, phase_amount = None):
+        # Augment data to add noise to it for training a model invariant for noise
+        # Add  noise to every observation within each sample
+        if jitter:
+            if subset_range is not None:
+                for i in subset_range:
+                    for j in range(1):
+                        pass
+        else:
+            for j in range(self.__len()):
+                for i in range(len(yData)):
+                    XData[j][i] += np.random.rand(jitter_range)
+            
+        # Add noise to every observation in a sample
+        if phase:
+            for i in range(len(yData)):
+                if subset:
+                    pass
+                else:
+                    XData[i] += phase_amount
+    
+        return XData
--- DataLoader.py
+++ DataLoader.py
@@ -9,66 +9,39 @@ import torch.fft as fft
 # Seed for reproducability
 np.random.seed(32)
 
-def augment_Data(XData, yData,
-                 jitter = False, jitter_amount = None,
-                 phase = False, phase_amount = None):
-    # Add random noise to every observation within each sample
-    if jitter:
-        for j in range(len(yData)):
-            for i in range(len(yData)):
-                XData[j][i] += np.random.rand(jitter_amount)
-    # Add noise to every observation in a sample
-    if phase:
-        for i in range(len(yData)):
-            XData[i] += phase_amount
-    
-    return XData
-
-
 class TimeSeriesDataset(Dataset):
     # Initializing this class requires that the parameter 'dataset' is inserted as an already loaded torch tensor using torch.load
     def __init__(self, dataset,
                  augment = False,
                  jitter = False, phase = False):
-        self.X = dataset['samples'].squeeze(1)
+        self.X = dataset['samples']
         self.y = dataset['labels']
+
+        # Make sure the channel is in the second dimension
+        if SleepEEG.X.shape.index(min(SleepEEG.X.shape)) != 1:
+            self.X = self.X.permute(0, 2, 1)
 
         if augment: ## Only augment data if we ask for it to be augmented
             raise(NotImplementedError)
                 
         # Transfer data to frequency domain using torch.fft (frequency fourier transform)
         self.X_f = fft.fft(self.X)#.abs()
-        self.X_aug_f = fft.fft(self.X_aug)
+        #self.X_aug_f = fft.fft(self.X_aug)
     
     def __len__(self):
         # Return the length of the dataset
         return len(self.y)
 
-    def plot_sample(self, dataset):
-        # Plot a random sample from the dataset
-        plt.plot(dataset[np.random.random_integers(self.__len__())])
+    def plot_sample(self):
+        # Plot a random sample from the dataset in time- and frequency domain
+        fig, ax = plt.subplots(1,2)
+        ax[0].plot(self.X[np.random.randint(self.__len__())])
+        ax[0].set_title("Time domain")
+        ax[1].plot(self.X_f[np.random.randint(self.__len__())])
+        ax[1].set_title("Frequency domain")
+        plt.show()
 
-    def augment_Data(self, XData, yData, subset_range = None,
-             jitter = False, jitter_range = None,
-             phase = False, phase_amount = None):
-        # Augment data to add noise to it for training a model invariant for noise
-            
-        # Add  noise to every observation within each sample
-        if jitter:
-            if subset_range is not None:
-                for i in range(subset_range):
-                    for j in range(1)
-            else:
-                for j in range(self.__len()):
-                    for i in range(len(yData)):
-                        XData[j][i] += np.random.rand(jitter_range)
-            
-        # Add noise to every observation in a sample
-        if phase:
-            for i in range(len(yData)):
-                if subset:
-                    pass
-                else:
-                    XData[i] += phase_amount
-    
-        return XData
+SleepEEG = TimeSeriesDataset(dataset=torch.load(os.path.join("datasets", "SleepEEG", "train.pt")))
+#SleepEEG.plot_sample()
+print(len(SleepEEG.X.shape))
+print(SleepEEG.X.shape.index(min(SleepEEG.X.shape)))

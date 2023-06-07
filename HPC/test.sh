061930bf6a78ec7c767f0e9631e1bbcf6f25de8d
LongVanNgo <longvanngo@hotmail.com>
2023-06-07 13:15:59 +0200

    Updated test.sh

--- HPC/test.sh
+++ HPC/test.sh
@@ -10,8 +10,9 @@
 
 # execute our command
 
+cd ..
 set -e
-source ../.venv/bin/activate
+source .venv/bin/activate
 #pip3 freeze
 #module load scipy/1.9.1-python-3.9.14
-python3 ../main.py
+python3 main.py

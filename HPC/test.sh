a4f5eb594229a473660b62998b45d05da42548e0
LongVanNgo <longvanngo@hotmail.com>
2023-06-07 13:05:37 +0200

    Added test shell script

---
+++ HPC/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J TFC
+#BSUB -o TFC_LOG_%J.out  
+#BSUB -q hpc
+#BSUB -W 48:00
+#BSUB -R "rusage[mem=8GB]"
+#BSUB -n 12
+#BSUB -R "span[hosts=1]"
+# all  BSUB option comments should be above this line!
+
+# execute our command
+
+set -e
+source ../.venv/bin/activate
+#pip3 freeze
+#module load scipy/1.9.1-python-3.9.14
+python3 main.py
